278ff4ad2c70294f71c6fb22f05c29af51790a0e
Fizo55 <hugomacees56000@gmail.com>
2024-07-01 15:15:07 +0200

    feat: add port update

--- src/MulticlientCreator/Program.cs
+++ src/MulticlientCreator/Program.cs
@@ -1,7 +1,6 @@
 using MulticlientCreator.Helpers;
 using System;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace MulticlientCreator
@@ -10,6 +9,7 @@ namespace MulticlientCreator
     {
         private const string OfficialName = "NostaleClientX.exe";
         private const string Pattern = "0C00000037392E3131302E38342E373500000000";
+        private const string PortPattern = "00A00F0000A10F0000A20F0000A00F0000A00F0000A00F0000A30F0000000000000000000000000000";
 
         public static void Main(string[] args)
         {
@@ -24,13 +24,22 @@ namespace MulticlientCreator
             var newIpPattern = string.Empty;
             if (fileString.Contains(Pattern))
             {
-                WriteMessage(ConsoleColor.DarkGreen, "Pattern found !");
+                WriteMessage(ConsoleColor.DarkGreen, "Ip pattern found !");
                 var ip = PromptForIpAddress();
                 newIpPattern = GenerateIpPattern(ip);
             }
 
-            var finder = new HexFinder(finalPath, newIpPattern);
-            if (finder.ReplaceIpPattern(Pattern))
+            var newPortPattern = string.Empty;
+            if (fileString.Contains(PortPattern))
+            {
+                WriteMessage(ConsoleColor.DarkGreen, "Port pattern found !");
+                var port = PromptForPort();
+                newPortPattern = GeneratePortPattern(port);
+                Console.WriteLine($"Port pattern: {newPortPattern}");
+            }
+
+            var finder = new HexFinder(finalPath, newIpPattern, newPortPattern);
+            if (finder.ReplaceIpPattern(Pattern, PortPattern))
             {
                 WriteMessage(ConsoleColor.DarkGreen, "Your multiclient has been successfully generated !");
             }
@@ -52,7 +61,13 @@ namespace MulticlientCreator
 
         private static string PromptForIpAddress()
         {
-            WriteMessage(ConsoleColor.Magenta, "Enter your VPS IP: ");
+            WriteMessage(ConsoleColor.Magenta, "Enter your ip: ");
+            return Console.ReadLine()!;
+        }
+        
+        private static string PromptForPort()
+        {
+            WriteMessage(ConsoleColor.Magenta, "Enter your port: ");
             return Console.ReadLine()!;
         }
 
@@ -60,6 +75,25 @@ namespace MulticlientCreator
         {
             using var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
             return DeserializationHelper.ReadFully(fileStream);
+        }
+
+        private static string GeneratePortPattern(string basePort)
+        {
+            int port = int.Parse(basePort);
+            string hexPort = port.ToString("X4");
+            string hexPortRearranged = hexPort.Substring(2, 2) + hexPort[..2];
+            var builder = new StringBuilder();
+
+            builder.Append("00");
+            
+            for (int i = 0; i < 7; i++)
+            {
+                builder.Append(hexPortRearranged).Append("0000");
+            }
+
+            builder.Append("000000000000000000000000");
+
+            return builder.ToString();
         }
 
         private static string GenerateIpPattern(string ip)
