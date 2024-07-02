54dff8384fcb382fdd44ce3f21516056e92ac1b0
Fizo55 <hugomacees56000@gmail.com>
2024-07-02 17:16:51 +0200

    feat: add GUI thanks to Kanzaki

---
+++ src/MulticlientCreator/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace MulticlientCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            lblNostalePath = new Label();
+            lblIP = new Label();
+            lblPort = new Label();
+            txtNostalePath = new TextBox();
+            btnBrowse = new Button();
+            btnGenerate = new Button();
+            txtIP = new TextBox();
+            txtPort = new TextBox();
+            madeBy = new Label();
+            lblFileName = new Label();
+            txtFileName = new TextBox();
+            SuspendLayout();
+            // 
+            // lblNostalePath
+            // 
+            lblNostalePath.AutoSize = true;
+            lblNostalePath.BackColor = SystemColors.ButtonHighlight;
+            lblNostalePath.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblNostalePath.ForeColor = Color.Black;
+            lblNostalePath.Location = new Point(9, 31);
+            lblNostalePath.Name = "lblNostalePath";
+            lblNostalePath.Size = new Size(116, 15);
+            lblNostalePath.TabIndex = 0;
+            lblNostalePath.Text = "NostaleClientX.exe:";
+            // 
+            // lblIP
+            // 
+            lblIP.AutoSize = true;
+            lblIP.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblIP.Location = new Point(53, 58);
+            lblIP.Name = "lblIP";
+            lblIP.Size = new Size(21, 15);
+            lblIP.TabIndex = 3;
+            lblIP.Text = "IP:";
+            // 
+            // lblPort
+            // 
+            lblPort.AutoSize = true;
+            lblPort.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblPort.Location = new Point(40, 85);
+            lblPort.Name = "lblPort";
+            lblPort.Size = new Size(34, 15);
+            lblPort.TabIndex = 5;
+            lblPort.Text = "Port:";
+            // 
+            // txtNostalePath
+            // 
+            txtNostalePath.Location = new Point(131, 26);
+            txtNostalePath.Name = "txtNostalePath";
+            txtNostalePath.Size = new Size(263, 23);
+            txtNostalePath.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            btnBrowse.Location = new Point(400, 26);
+            btnBrowse.Name = "btnBrowse";
+            btnBrowse.Size = new Size(70, 24);
+            btnBrowse.TabIndex = 2;
+            btnBrowse.Text = "Browse...";
+            btnBrowse.UseVisualStyleBackColor = true;
+            btnBrowse.Click += btnBrowse_Click;
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.BackColor = Color.FromArgb(64, 64, 64);
+            btnGenerate.ForeColor = Color.White;
+            btnGenerate.Location = new Point(30, 160);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(457, 38);
+            btnGenerate.TabIndex = 7;
+            btnGenerate.Text = "Generate Multiclient";
+            btnGenerate.UseVisualStyleBackColor = false;
+            btnGenerate.Click += btnGenerate_Click;
+            // 
+            // txtIP
+            // 
+            txtIP.ForeColor = SystemColors.GrayText;
+            txtIP.Location = new Point(80, 55);
+            txtIP.Name = "txtIP";
+            txtIP.Size = new Size(176, 23);
+            txtIP.TabIndex = 4;
+            txtIP.Text = "Enter IP address";
+            txtIP.Enter += txtIP_Enter;
+            txtIP.Leave += txtIP_Leave;
+            // 
+            // txtPort
+            // 
+            txtPort.ForeColor = SystemColors.GrayText;
+            txtPort.Location = new Point(80, 82);
+            txtPort.Name = "txtPort";
+            txtPort.Size = new Size(88, 23);
+            txtPort.TabIndex = 6;
+            txtPort.Text = "Enter port";
+            txtPort.Enter += txtPort_Enter;
+            txtPort.Leave += txtPort_Leave;
+            // 
+            // madeBy
+            // 
+            madeBy.BackColor = Color.Transparent;
+            madeBy.Font = new Font("Arial Rounded MT Bold", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            madeBy.ForeColor = SystemColors.ActiveCaptionText;
+            madeBy.Location = new Point(444, 306);
+            madeBy.Name = "madeBy";
+            madeBy.Size = new Size(109, 23);
+            madeBy.TabIndex = 8;
+            madeBy.Text = "Made by Fizo55.";
+            // 
+            // lblFileName
+            // 
+            lblFileName.AutoSize = true;
+            lblFileName.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblFileName.Location = new Point(9, 111);
+            lblFileName.Name = "lblFileName";
+            lblFileName.Size = new Size(65, 15);
+            lblFileName.TabIndex = 6;
+            lblFileName.Text = "File Name:";
+            // 
+            // txtFileName
+            // 
+            txtFileName.Location = new Point(80, 108);
+            txtFileName.Name = "txtFileName";
+            txtFileName.Size = new Size(264, 23);
+            txtFileName.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(542, 319);
+            Controls.Add(btnGenerate);
+            Controls.Add(txtPort);
+            Controls.Add(lblPort);
+            Controls.Add(txtIP);
+            Controls.Add(lblIP);
+            Controls.Add(btnBrowse);
+            Controls.Add(txtNostalePath);
+            Controls.Add(lblNostalePath);
+            Controls.Add(madeBy);
+            Controls.Add(txtFileName);
+            Controls.Add(lblFileName);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MulticlientCreator by Fizo";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox txtFileName;
+        private Label lblFileName;
+        private Label lblNostalePath;
+        private TextBox txtNostalePath;
+        private Button btnBrowse;
+        private Label lblIP;
+        private TextBox txtIP;
+        private Label lblPort;
+        private TextBox txtPort;
+        private Button btnGenerate;
+        private Label madeBy;
+    }
+}
--- src/MulticlientCreator/Program.cs
+++ src/MulticlientCreator/Program.cs
@@ -1,129 +1,17 @@
-using MulticlientCreator.Helpers;
-using System;
-using System.IO;
-using System.Text;
-
 namespace MulticlientCreator
 {
-    public class Program
+    internal static class Program
     {
-        private const string OfficialName = "NostaleClientX.exe";
-        private const string Pattern = "0C00000037392E3131302E38342E373500000000";
-        private const string PortPattern = "00A00F0000A10F0000A20F0000A00F0000A00F0000A00F0000A30F0000000000000000000000000000";
-
-        public static void Main(string[] args)
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            Console.Title = "MulticlientCreator by Fizo";
-
-            var nostalePath = PromptForNostalePath();
-            var finalPath = Path.Combine(nostalePath, OfficialName);
-
-            byte[] fileBytes = ReadBytesFromFile(finalPath);
-            string fileString = HexHelper.ToHexString(fileBytes);
-
-            var newIpPattern = string.Empty;
-            if (fileString.Contains(Pattern))
-            {
-                WriteMessage(ConsoleColor.DarkGreen, "Ip pattern found !");
-                var ip = PromptForIpAddress();
-                newIpPattern = GenerateIpPattern(ip);
-            }
-
-            var newPortPattern = string.Empty;
-            if (fileString.Contains(PortPattern))
-            {
-                WriteMessage(ConsoleColor.DarkGreen, "Port pattern found !");
-                var port = PromptForPort();
-                newPortPattern = GeneratePortPattern(port);
-                Console.WriteLine($"Port pattern: {newPortPattern}");
-            }
-
-            var finder = new HexFinder(finalPath, newIpPattern, newPortPattern);
-            if (finder.ReplaceIpPattern(Pattern, PortPattern))
-            {
-                WriteMessage(ConsoleColor.DarkGreen, "Your multiclient has been successfully generated !");
-            }
-
-            Console.ReadLine();
-        }
-
-        private static string PromptForNostalePath()
-        {
-            WriteMessage(ConsoleColor.Magenta, "Enter your nostale path:");
-            string nostalePath;
-            do
-            {
-                nostalePath = Console.ReadLine()!;
-            } while (string.IsNullOrEmpty(nostalePath) || !File.Exists(Path.Combine(nostalePath, "NostaleClientX.exe")));
-
-            return nostalePath;
-        }
-
-        private static string PromptForIpAddress()
-        {
-            WriteMessage(ConsoleColor.Magenta, "Enter your ip: ");
-            return Console.ReadLine()!;
-        }
-        
-        private static string PromptForPort()
-        {
-            WriteMessage(ConsoleColor.Magenta, "Enter your port: ");
-            return Console.ReadLine()!;
-        }
-
-        private static byte[] ReadBytesFromFile(string path)
-        {
-            using var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
-            return DeserializationHelper.ReadFully(fileStream);
-        }
-
-        private static string GeneratePortPattern(string basePort)
-        {
-            int port = int.Parse(basePort);
-            string hexPort = port.ToString("X4");
-            string hexPortRearranged = hexPort.Substring(2, 2) + hexPort[..2];
-            var builder = new StringBuilder();
-
-            builder.Append("00");
-            
-            for (int i = 0; i < 7; i++)
-            {
-                builder.Append(hexPortRearranged).Append("0000");
-            }
-
-            builder.Append("000000000000000000000000");
-
-            return builder.ToString();
-        }
-
-        private static string GenerateIpPattern(string ip)
-        {
-            var split = ip.Split(".");
-            var builder = new StringBuilder();
-            builder.Append("0" + Convert.ToString(ip.Length, 16).ToUpper() + "000000");
-
-            for (var i = 0; i < 4; i++)
-            {
-                builder.Append(HexHelper.ToHexString(split[i]));
-
-                if (i == 3) break;
-
-                builder.Append("2E"); // this is the dot "."
-            }
-
-            for (var j = builder.Length; j < 40; j++)
-            {
-                builder.Append("0");
-            }
-
-            return builder.ToString();
-        }
-
-        public static void WriteMessage(ConsoleColor color, string message)
-        {
-            Console.ForegroundColor = color;
-            Console.WriteLine(message);
-            Console.ResetColor();
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
         }
     }
 }
